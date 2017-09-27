0f90a3f70aeba4a8769dd7a2c317ffe0d16e1d1a
frans.lundberg@assaabloy.com <frans.lundberg@assaabloy.com>
2017-09-27 09:00:33 +0200

    comment, new sig prefix, SC-SIG01...

--- src/saltchannel/v2/V2Util.java
+++ src/saltchannel/v2/V2Util.java
@@ -10,11 +10,11 @@ public class V2Util {
     private static SaltLib salt = SaltLibFactory.getLib();
     
     public static final byte[] SIG1_PREFIX = new byte[] {
-            0x53, 0x41, 0x4c, 0x54, 0x53, 0x49, 0x47, 0x31 };
-            // "SALTSIG1" in ASCII
+            0x53, 0x41, 0x4c, 0x54, 0x53, 0x49, 0x47, 0x31 };   // XXX
+            // "SC-SIG01" in ASCII
     public static final byte[] SIG2_PREFIX = new byte[] {
-            0x53, 0x41, 0x4c, 0x54, 0x53, 0x49, 0x47, 0x32 };   
-            // "SALTSIG2" in ASCII
+            0x53, 0x41, 0x4c, 0x54, 0x53, 0x49, 0x47, 0x32 };     // XXX
+            // "SC-SIG02" in ASCII
 
     public static byte[] createSignature(KeyPair sigKeyPair, byte[]... arrays) {
         byte[] message = concat(arrays);
