ec8a1ce8a7acd2b92a4da37c963e394749137ee5
frans.lundberg@assaabloy.com <frans.lundberg@assaabloy.com>
2017-10-10 07:42:57 +0200

    improved size checks when parsing

--- src/saltchannel/util/Deserializer.java
+++ src/saltchannel/util/Deserializer.java
@@ -1,16 +1,12 @@
 package saltchannel.util;
 
 import java.io.UnsupportedEncodingException;
-
 import saltchannel.BadPeer;
 import saltchannel.v2.packets.PacketHeader;
 
 /**
  * A deserializer that can deserialize (read) bytes created with 
- * Serializer. Note, this class does not throw BadPeer. Its methods
- * throw IllegalArgumentException, ArrayIndexOutOfBoundsException and such.
- * This caller should either make sure such exceptions are not thrown
- * or catch them to detect a bad peer.
+ * Serializer. Throws BadPeer when deserialization cannot be done.
  * 
  * @author Frans Lundberg
  */
@@ -18,13 +14,6 @@ public class Deserializer {
     private byte[] buffer;
     private int firstOffset;
     private int offset;
-    private int bitOffset;
-
-    private static final int[] BITS = {
-            1, 2, 4, 8, 16, 32, 64, 128
-    };
-    
-    private static final int INT4_MASK = 1 + 2 + 4 + 8;
     
     public Deserializer(byte[] buffer, int offset) {
         if (offset > buffer.length) {
@@ -34,7 +23,6 @@ public class Deserializer {
         this.buffer = buffer;
         this.firstOffset = offset;
         this.offset = offset;
-        this.bitOffset = 0;
     }
     
     
@@ -47,9 +35,8 @@ public class Deserializer {
     }
     
     public byte readByte() {
-        checkBitOffsetZero();
-        if (offset > buffer.length - 1) {
-            throw new BadPeer("offset out of bounds, " + offset);
+        if (bytesLeft() < 1) {
+            throw new BadPeer("no bytes left, " + offset);
         }
         
         byte result = buffer[offset];
@@ -62,13 +49,8 @@ public class Deserializer {
     }
     
     public byte[] readBytes(int size) {
-        checkBitOffsetZero();
-        if (size < 0) {
-            throw new IllegalArgumentException("bad size");
-        }
-        
-        if (size > buffer.length) {
-            throw new IllegalArgumentException("size too large");
+        if (bytesLeft() < size) {
+            throw new BadPeer("out of bounds, cannot read " + size + " bytes");
         }
         
         byte[] result = new byte[size];
@@ -91,64 +73,46 @@ public class Deserializer {
         }
     }
     
-    public boolean readBit() {
-        int value = Bytes.unsigned(buffer[offset]);
-        int mask = BITS[bitOffset];
-        incrementBitOffset();
-        return (value & mask) != 0;
-    }
-    
-    public int readBitAsInt() {
-        return readBit() ? 1 : 0;
-    }
-    
     public PacketHeader readHeader() {
+        if (bytesLeft() < PacketHeader.SIZE) {
+            throw new BadPeer("out of bounds, cannot read header");
+        }
         PacketHeader header = new PacketHeader(buffer, offset);
         offset += PacketHeader.SIZE;
         return header;
     }
     
-    /**
-     * Reads a 4-bit unsigned integer, range 0-15, from the first 4 bits of the 
-     * current byte.
-     */
-    public int readUint4() {
-        checkBitOffsetZero();
-        
-        int result = Bytes.unsigned(buffer[offset]) & INT4_MASK;
-        bitOffset += 4;
-        return result;
-    }
-    
     public int readUint16() {
+        if (bytesLeft() < 2) {
+            throw new BadPeer("out of bounds, cannot read uint16");
+        }
+        
         int result = Bytes.bytesToUShortLE(buffer, offset);
         offset += 2;
         return result;
     }
     
     public int readInt32() {
+        if (bytesLeft() < 4) {
+            throw new BadPeer("out of bounds, cannot read int32");
+        }
+        
         int result = Bytes.bytesToIntLE(buffer, offset);
         offset += 4;
         return result;
     }
     
     public long readInt64() {
+        if (bytesLeft() < 4) {
+            throw new BadPeer("out of bounds, cannot read int64");
+        }
+        
         long result = Bytes.bytesToLongLE(buffer, offset);
         offset += 8;
         return result;
     }
     
-    private void checkBitOffsetZero() {
-        if (bitOffset != 0) {
-            throw new IllegalStateException("bitOffset not zero, " + bitOffset);
-        }
-    }
-    
-    private void incrementBitOffset() {
-        bitOffset += 1;
-        if (bitOffset == 8) {
-            bitOffset = 0;
-            offset += 1;
-        }
+    private int bytesLeft() {
+        return buffer.length - offset;
     }
 }
