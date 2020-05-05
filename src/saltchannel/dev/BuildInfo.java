c302a3795a0fd9889547c04ebe1d8bac1aef60cc
Frans Lundberg <frans.lundberg@assaabloy.com>
2020-05-05 14:51:02 +0200

    Release 2.6, fixed build script (Attempt 2)

--- src/saltchannel/dev/BuildInfo.java
+++ src/saltchannel/dev/BuildInfo.java
@@ -9,12 +9,12 @@ public class BuildInfo {
     
     private BuildInfo() {}
     
-    public static final String VERSION = identity("2.5");
+    public static final String VERSION = identity("2.6");
     
     /**
      * Build time, for example: "160508.1435".
      */
-    public static final String TIME = identity("20200505.1445");
+    public static final String TIME = identity("20200505.1449");
         
     /**
      * Returns 's'; useful for avoiding compiler inlining of a final field.
